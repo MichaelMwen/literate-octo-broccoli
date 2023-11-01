9c7801f75eb01d7b5ce7137e96201ad9f70b4d07
MichaelMwen <105641573+MichaelMwen@users.noreply.github.com>
2023-11-01 11:31:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+/**
+ * theme change
+ */
+(function () {
+  [...document.querySelectorAll(".control")].forEach(button => {
+      button.addEventListener("click", function() {
+          document.querySelector(".active-btn").classList.remove("active-btn");
+          this.classList.add("active-btn");
+          document.querySelector(".active").classList.remove("active");
+          document.getElementById(button.dataset.id).classList.add("active");
+      })
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
+  })
+})();
+
+/* ========== CV download script starts ========== */
+
+$(document).ready(function() {
+  $('#btnDownload').click(function(e) {
+      e.preventDefault();
+      window.location.href = "D:\MatLab\PortFolio\portfolio-master\assets\images\cv.docx";
+  });
+});
+
+
+/*==Blog==*/
+$(document).ready(function() {
+  $('#img-cover').click(function(e) {
+      e.preventDefault();
+      window.location.href = "D:\MatLab\PortFolio\portfolio-master\assets\images\Beige photo essay.pdf";
+  });
+});
